6674f89d56ba2498ec34c5506737111340d7215b
CT-Lumar <74888228+CT-Lumar@users.noreply.github.com>
2020-11-29 19:10:46 +0800

    Create PyLadies_works.py

---
+++ PyLadies_works.py
@@ -0,0 +1,1054 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+print("hi"*7)
+
+
+# In[3]:
+
+
+70**0.5*10
+
+
+# In[4]:
+
+
+42//12
+
+
+# In[5]:
+
+
+my_score=96
+pi=3.1415967
+url="test"
+
+print(type(my_score))
+print(type(pi))
+print(type(url))
+
+
+# In[6]:
+
+
+print(type(print))
+
+
+# In[7]:
+
+
+a="70"
+b="50"
+print(int(a)+int(b))
+
+
+# In[8]:
+
+
+a=input("請輸入a~")
+b=input("請輸入b~")
+print(a+b)
+
+
+# In[9]:
+
+
+a=input("請輸入a~")
+b=input("請輸入b~")
+print(a+b)
+
+
+# In[10]:
+
+
+a=int(input("請輸入a~"))
+b=int(input("請輸入b~"))
+print(a+b)
+
+
+# In[11]:
+
+
+a = int(input("請輸入a~"))
+print("a*2",a*2)
+
+
+# In[12]:
+
+
+my_string = "PyLadies Taiwan"
+print(my_string.replace("a","A",5))
+
+
+# In[13]:
+
+
+my_string = "PyLadies Taiwan"
+print(my_string.replace("a","A",1))
+
+
+# In[14]:
+
+
+my_string = "PyLadies Taiwan Lumar"
+print(my_string.replace("a","A",2))
+
+
+# In[15]:
+
+
+my_string = "PyLadies Taiwan Lumar"
+print(my_string.replace("a","A",3))
+
+
+# In[16]:
+
+
+my_string = "PyLadies Taiwan Lumar hahaha"
+print(my_string.replace("a","A",5))
+
+
+# In[17]:
+
+
+article = """2012年我創立了一間公司 來教女孩寫程式 我發現到，透過教程式編寫 其實我是在培養她們變得勇敢 
+程式編寫是一個「不斷檢測 與發現錯誤」的無止境過程 
+要設法在對的地方，輸入正確的指令 有時候只是差一個分號 就足以決定成敗 
+程式碼錯了，就全盤皆錯 時常要嘗試很多很多次 奇蹟才會出現 
+你試著建構的東西總算活了起來 這項工作需要毅力 需要不完美
+
+我們馬上就發現 參與學程的女孩都害怕犯錯 害怕不完美 「寫程式的女孩」計畫的每個老師 都告訴我一樣的故事 
+第一週，女孩們還在學 怎麼編寫程式碼時 學生會請她過去跟她說 「我不知道要寫什麼程式碼」 
+老師看了看她的螢幕 只會看到一片空白 
+假如她不夠瞭解，她會覺得她的學生 花了20分鐘，只是盯著螢幕發呆 
+但如果她多按幾下復原鍵 她會看見學生其實 寫下程式碼又刪除了 
+她試過，她逼近了 可是還沒做到完全正確 
+她不呈現編寫的進展 卻寧願給人看一片空白 寧缺勿濫"""
+print(artical.replace("女孩" "女生"))
+
+
+# In[18]:
+
+
+article = """2012年我創立了一間公司 來教女孩寫程式 我發現到，透過教程式編寫 其實我是在培養她們變得勇敢 
+程式編寫是一個「不斷檢測 與發現錯誤」的無止境過程 
+要設法在對的地方，輸入正確的指令 有時候只是差一個分號 就足以決定成敗 
+程式碼錯了，就全盤皆錯 時常要嘗試很多很多次 奇蹟才會出現 
+你試著建構的東西總算活了起來 這項工作需要毅力 需要不完美
+
+我們馬上就發現 參與學程的女孩都害怕犯錯 害怕不完美 「寫程式的女孩」計畫的每個老師 都告訴我一樣的故事 
+第一週，女孩們還在學 怎麼編寫程式碼時 學生會請她過去跟她說 「我不知道要寫什麼程式碼」 
+老師看了看她的螢幕 只會看到一片空白 
+假如她不夠瞭解，她會覺得她的學生 花了20分鐘，只是盯著螢幕發呆 
+但如果她多按幾下復原鍵 她會看見學生其實 寫下程式碼又刪除了 
+她試過，她逼近了 可是還沒做到完全正確 
+她不呈現編寫的進展 卻寧願給人看一片空白 寧缺勿濫"""
+print(article.replace("女孩" "女生"))
+
+
+# In[19]:
+
+
+article = """2012年我創立了一間公司 來教女孩寫程式 我發現到，透過教程式編寫 其實我是在培養她們變得勇敢 
+程式編寫是一個「不斷檢測 與發現錯誤」的無止境過程 
+要設法在對的地方，輸入正確的指令 有時候只是差一個分號 就足以決定成敗 
+程式碼錯了，就全盤皆錯 時常要嘗試很多很多次 奇蹟才會出現 
+你試著建構的東西總算活了起來 這項工作需要毅力 需要不完美
+
+我們馬上就發現 參與學程的女孩都害怕犯錯 害怕不完美 「寫程式的女孩」計畫的每個老師 都告訴我一樣的故事 
+第一週，女孩們還在學 怎麼編寫程式碼時 學生會請她過去跟她說 「我不知道要寫什麼程式碼」 
+老師看了看她的螢幕 只會看到一片空白 
+假如她不夠瞭解，她會覺得她的學生 花了20分鐘，只是盯著螢幕發呆 
+但如果她多按幾下復原鍵 她會看見學生其實 寫下程式碼又刪除了 
+她試過，她逼近了 可是還沒做到完全正確 
+她不呈現編寫的進展 卻寧願給人看一片空白 寧缺勿濫"""
+print(article.replace("女孩","女生"))
+
+
+# In[ ]:
+
+
+
+
+
+# In[20]:
+
+
+test="www.google{}hahaha".format(.)
+print(test)
+
+
+# In[21]:
+
+
+test="www.google{}hahaha".format(annoying)
+print(test)
+
+
+# In[22]:
+
+
+test="www.google{}hahaha".format(".")
+print(test)
+
+
+# In[23]:
+
+
+print("我是PyLadies的{}，我喜歡{}，很高興認識大家！".format("Lumar","吃"))
+
+
+# In[24]:
+
+
+a=str(input(姓名))
+b=str(input(興趣))
+print("我是PyLadies的{}，我喜歡{}，很高興認識大家！".format(a,b))
+
+
+# In[25]:
+
+
+a=str(input("輸入姓名"))
+b=str(input("輸入興趣"))
+print("我是PyLadies的{}，我喜歡{}，很高興認識大家！".format(a,b))
+
+
+# In[26]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString/2+1))
+
+
+# In[27]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString/2+1))
+
+
+# In[28]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString//2))
+
+
+# In[29]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString/2+1))
+
+
+# In[30]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString//2))
+
+
+# In[31]:
+
+
+12//5
+
+
+# In[32]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString//2+1))
+
+
+# In[33]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString//2))
+
+
+# In[34]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString//2+1))
+
+
+# In[35]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString//2))
+
+
+# In[36]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString//2))
+
+
+# In[37]:
+
+
+bool(0)
+bool(3)12
+
+
+# In[38]:
+
+
+15//2
+
+
+# In[39]:
+
+
+15%2
+
+
+# In[40]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString%2))
+
+
+# In[41]:
+
+
+InputString=int(input("輸入數值"))
+print(bool(InputString%2))
+
+
+# In[42]:
+
+
+Now_h=int(input"現在幾時")*60
+Now_m=int(input"現在幾分")
+Last_h=int(input"末班車幾時")*60
+Last_m=int(input"末班車幾分")
+print(Last_h+Last_m <= 13+Now_h+Now_m)
+
+
+# In[43]:
+
+
+Now_h=int(input"現在幾時")
+Now_m=int(input"現在幾分")
+Last_h=int(input"末班車幾時")
+Last_m=int(input"末班車幾分")
+print(Last_h*60+Last_m <= 13+Now_h*60+Now_m)
+
+
+# In[44]:
+
+
+Now_h=int(input("現在幾時"))*60
+Now_m=int(input("現在幾分"))
+Last_h=int(input("末班車幾時"))*60
+Last_m=int(input("末班車幾分"))
+print(Last_h+Last_m <= 13+Now_h+Now_m)
+
+
+# In[45]:
+
+
+Now_h=int(input("現在幾時"))*60
+Now_m=int(input("現在幾分"))
+Last_h=int(input("末班車幾時"))*60
+Last_m=int(input("末班車幾分"))
+print(Last_h+Last_m <= 13+Now_h+Now_m)
+
+
+# In[46]:
+
+
+Now_h=int(input("現在幾時"))*60
+Now_m=int(input("現在幾分"))
+Last_h=int(input("末班車幾時"))*60
+Last_m=int(input("末班車幾分"))
+print(Last_h+Last_m >= 13+Now_h+Now_m)
+
+
+# In[47]:
+
+
+gift = ""
+gift = "購物袋"
+gift = gift+"購物袋"
+
+
+# In[48]:
+
+
+gift = ""
+print(gift)
+gift = "購物袋"
+print(gift)
+gift = gift+"購物袋"
+print(gift)
+
+
+# In[49]:
+
+
+print(gift)
+
+
+# In[50]:
+
+
+gift = ""
+print(gift)
+
+
+# In[51]:
+
+
+time="2205"
+if    int(time) > int("2200"):
+    print("現在很晚了")
+    print("怎麼還在外面遊蕩")
+
+
+# In[52]:
+
+
+time="0305"
+if    int(time) > int("2200"):
+    print("現在很晚了")
+    print("怎麼還在外面遊蕩")
+elif    int(time) < int("0500"):
+    print("現在很晚了2")
+    print("怎麼還在外面遊蕩2")
+
+
+# In[53]:
+
+
+time="0305"
+if    int(time) > int("2200"):
+    print("現在很晚了")
+    print("怎麼還在外面遊蕩")
+elif    int(time) < int("0500"):
+    print("現在很晚了2")
+    print("怎麼還在外面遊蕩2")
+
+
+# In[54]:
+
+
+time="2100"
+if    int(time) > int("2200"):
+    print("現在很晚了")
+    print("怎麼還在外面遊蕩")
+elif    int(time) < int("0500"):
+    print("現在很晚了2")
+    print("怎麼還在外面遊蕩2")
+
+
+# In[55]:
+
+
+time="2300"
+if    int(time) > int("2200"):
+    print("現在很晚了")
+    print("怎麼還在外面遊蕩")
+elif    int(time) < int("0500"):
+    print("現在很晚了2")
+    print("怎麼還在外面遊蕩2")
+
+
+# In[56]:
+
+
+*time(="2300")
+if    int(time) > int("2200"):
+    print("現在很晚了")
+    print("怎麼還在外面遊蕩")
+elif    int(time) < int("0500"):
+    print("現在很晚了2")
+    print("怎麼還在外面遊蕩2")*/
+
+
+# In[57]:
+
+
+Year=int(input("輸入西元"))
+if    Year//400=Year//4 not Year//100:
+    print(str(Year)+"是閏年")
+
+
+# In[58]:
+
+
+Year=int(input("輸入西元"))
+if    Year//400=Year//4!=Year//100:
+    print(str(Year)+"是閏年")
+
+
+# In[59]:
+
+
+Year=int(input("輸入西元"))
+if    Year//400=Year//4 and Year//100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[60]:
+
+
+Year=int(input("輸入西元"))
+if  Year//400=Year//4 and Year//100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[61]:
+
+
+Year=int(input("輸入西元"))
+if  Year//400=Year//4 and Year//100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[62]:
+
+
+Year=int(input("輸入西元"))
+if  Year%400=Year/%4 and Year&100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[63]:
+
+
+Year=int(input("輸入西元"))
+if  Year%400=Year%4 and Year%100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[64]:
+
+
+Year=int(input("輸入西元"))
+if  Year%400=Year%4 && Year%100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[65]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400=Year%4 and Year%100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[66]:
+
+
+Year=int(input("輸入西元"))
+
+
+# In[67]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 and Year%100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[68]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 && Year%100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[69]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 and Year%100!=0:
+    print(str(Year)+"是閏年")
+
+
+# In[70]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 && Year%100!=0:
+    print(str(Year)+"是閏年")
+    else:
+        print("duh")
+
+
+# In[71]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 and Year%100!=0:
+    print(str(Year)+"是閏年")
+    else:
+        print("duh")
+
+
+# In[72]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 and Year%100!=0:
+    print(str(Year)+"是閏年")
+    else
+        print("duh")
+
+
+# In[74]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 and Year%100!=0:
+        print(str(Year)+"是閏年")
+    else
+        print("duh")
+
+
+# In[75]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 and Year%100!=0:
+    print(str(Year)+"是閏年")
+    else:
+        print("duh")
+
+
+# In[76]:
+
+
+Year=int(input("輸入西元"))
+if    Year%400==Year%4 and Year%100!=0:
+    print(str(Year)+"是閏年")
+else:
+    print("duh")
+
+
+# In[77]:
+
+
+x
+
+
+# In[78]:
+
+
+people = [145,148,151,153,158,161,163,164,166,168,170,172,175,192]
+print(people[-1]-people[1])
+
+
+# In[79]:
+
+
+score_of_each_student = [85,70,54,87,98,66,40]
+print(max(score_of_each_student)-min(score_of_each_student))
+print(sum(score_of_each_student)/(len(score_of_each_student)))
+
+
+# In[80]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+print (article.count("in"))
+
+
+# In[81]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.split(" ")
+print(article.count("in"))
+
+
+# In[82]:
+
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.split(" ")
+print(article.count("in"))
+print(article.count("tea"))
+
+
+# In[83]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.split(" ")
+article = article.split(".")
+print(article.count("in"))
+print(article.count("tea"))
+
+
+# In[84]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.split(" ")
+article = article.split(.)
+print(article.count("in"))
+print(article.count("tea"))
+
+
+# In[85]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.split(" ")
+article = article.split('.')
+print(article.count("in"))
+print(article.count("tea"))
+
+
+# In[86]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.split(" ",.)
+print(article.count("in"))
+print(article.count("tea"))
+
+
+# In[87]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.split(" ",'.')
+print(article.count("in"))
+print(article.count("tea"))
+
+
+# In[88]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.replace("." " ")
+article = article.split(" ")
+print(article.count("in"))
+print(article.count("tea"))
+
+
+# In[89]:
+
+
+article = """
+Bubble tea represents the "QQ" food texture that Taiwanese love. 
+The phrase refers to something that is especially chewy, like the tapioca balls that form the 'bubbles' in bubble tea. 
+It's said this unusual drink was invented out of boredom. 
+"""
+article = article.replace(".", " ")
+article = article.split(" ")
+print(article.count("in"))
+print(article.count("tea"))
+
+
+# In[90]:
+
+
+l = [32,56,58,62,79,82,98]
+for i in range(0,len(l),1):
+    print("學生{}".format(i),i**0.5*10
+
+
+# In[91]:
+
+
+l = [32,56,58,62,79,82,98]
+for i in range(0,len(l),1):
+    print("學生{}".format(i),i**0.5*10)
+
+
+# In[92]:
+
+
+l = [32,56,58,62,79,82,98]
+for i in range(0,len(l),1):
+    print("學生{}".format(i+1),i**0.5*10)
+
+
+# In[93]:
+
+
+l = [32,56,58,62,79,82,98]
+print(len(l))
+for i in range(0,len(l),1):
+    print("學生{}".format(i+1),i**0.5*10)
+
+
+# In[94]:
+
+
+l = [32,56,58,62,79,82,98]
+print(len(l))
+for i in range(1,len(l),1):
+    print("學生{}".format(i+1),i**0.5*10)
+
+
+# In[95]:
+
+
+l = [32,56,58,62,79,82,98]
+print(len(l))
+for i in range(0,len(l),1):
+    print("學生{}".format(i+1),i**0.5*10)
+
+
+# In[96]:
+
+
+l = [32,56,58,62,79,82,98]
+print(len(l))
+for i in range(0,len(l)-1,1):
+    print("學生{}".format(i+1),i**0.5*10)
+
+
+# In[97]:
+
+
+l = [32,56,58,62,79,82,98]
+print(len(l))
+for i in range(0,len(l),1):
+    print("學生{}".format(i+1),i**0.5*10)
+
+
+# In[98]:
+
+
+l = [32,56,58,62,79,82,98]
+for i in range(0,len(l),1):
+    print("學生{}".format(i+1),l[i]**0.5*10)
+
+
+# In[99]:
+
+
+l = [32,56,58,62,79,82,98]
+for i in range(0,len(l),1):
+    print("學生{}".format(i+1),l[i]**0.5*10)
+
+
+# In[116]:
+
+
+l = [32,56,58,62,79,82,98]
+count=0
+new_l=""
+for i in l:
+    print("學生{}".format(count+1),i**0.5*10)
+    count=count+1
+    new_l=new_l+" "+str(i**0.5*10)
+print(new_l)
+
+
+# In[115]:
+
+
+l = [32,56,58,62,79,82,98]
+for i in range(0,len(l),1):
+    print("學生{}".format(i+1),l[i]**0.5*10)
+
+
+# In[125]:
+
+
+files = ["大阪城.jpg","大阪市夜景.jpg","京都塔水舞.mp4","京都清水寺01.jpg"]
+ToBeRemoved=[]
+for i in files:
+    if "大阪" in i:
+        ToBeRemoved.append(i)
+print(ToBeRemoved)
+
+
+# In[126]:
+
+
+l = [1,2,[3,4],2,3,4]
+print(l.pop())
+print(l)
+print(l.pop(2))
+print(l)
+
+
+# In[141]:
+
+
+l=[1,2,6,1,3,2,5]
+l1=[]
+for i in l:
+    if   i in l1:
+        print("test")
+    else:
+        l1.append(i)
+print(l1)
+    
+
+
+# In[150]:
+
+
+l = [32,56,58,62,79,82,98]
+total_score = 0
+for index in range(len(l)):
+    print("學生{}".format(index+1),l[index]**0.5*10)
+    total_score += l[index]
+print("總平均",total_score/len(l))
+
+
+# In[156]:
+
+
+s1 = "上海自來水來自海上"
+s1 = s1.replace(",","")
+s2 = ""
+print(s1)
+for index in range(len(s1)-1,0,-1):
+    s2+=s1[index]
+if s1==s2:
+    print("是回文")
+else:
+    print("不是回文")
+
+
+# In[159]:
+
+
+l = [["Mars",20],["姿君",25],["毛毛",10]]
+# Mars 儲值 15元, 再來是 姿君儲值 5元
+print(l[0][1])
+l[0][1]+=15
+l[1][1]+=5
+print(l)
+
+
+# In[168]:
+
+
+stock={
+    "banana":5,
+    "apple":3,
+    "orange":10
+}
+print(stock["banana"])
+price={
+    "banana":5,
+    "apple":20,
+    "orange":15
+}
+revenue=0
+revenue += stock["banana"] * price["banana"]
+revenue += stock["apple"] * price["apple"]
+revenue += stock["orange"] * price["orange"]
+print(revenue)
+revenue2=0
+for  fruits in stock:
+    print(fruits)
+    revenue2 += stock[fruits]*price[fruits]
+print(revenue2)
+
+
+# In[172]:
+
+
+product = {
+    "banana":{
+        "stock":5,
+        "price":5
+    },
+    "apple":{
+        "stock":3,
+        "price":20
+    },
+    "orange":{
+        "stock":10,
+        "price":15
+    }
+}
+for test in product:
+    print(product[test]["stock"])
+
+
+# In[186]:
+
+
+d = {"Mars":20,"姿君":25,"毛毛":10}
+l = [["Mars",15],["姿君",5]]
+for a1,a2 in l:
+    d[a1]=d[a1]+a2
+    #print(a1,a2)
+print(d)
+
+
+# In[198]:
+
+
+l = [["A",10],["B",5],["C",23],["B",7],["A",6],["A",3],["C",2]]
+d = {}
+for a1,a2 in l:
+    if a1 not in d:
+        d[a1]=0
+    d[a1]+=a2
+print(d)
+
+
+# In[ ]:
+
+
+
+
